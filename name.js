56ce7257727ee817630c2b9e08a5e77313bcf7d9
shehnila-wahab <160872560+shehnila-wahab@users.noreply.github.com>
2024-02-26 00:18:55 +0500

    EXERCISE 3
    TYPESCRIPT ASSIGMENT

---
+++ name.js
@@ -0,0 +1,15 @@
+"use strict";
+let personName = '';
+personName = prompt('what is your name?') || '';
+let lowerCase = personName.toLowerCase();
+let upperCase = personName.toUpperCase();
+let titleCase = personName.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+if (personName !== null && personName !== '') {
+    alert(`Hello ${personName}, here are your name in:
+    LowerCase: ${lowerCase}
+    UpperCase: ${upperCase}
+    TitleCase: ${titleCase}`);
+}
+else {
+    alert('please fill your name !');
+}
